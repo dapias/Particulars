4c9d152b97537ce35d672ad6fd6f9680dc361fe3
Vishnu V. Krishnan <vishnugb@gmail.com>
2017-10-26 19:29:04 +0530

    minor mods

--- check_eigen.f90
+++ check_eigen.f90
@@ -1,7 +1,7 @@
 PROGRAM check_eigen
 
     !USE la_precision, ONLY: WP => DP
-    USE f95_lapack, ONLY: LA_SYEVX
+    USE f95_lapack, ONLY: LA_SYEVR
 
     IMPLICIT NONE
 
@@ -9,13 +9,14 @@ PROGRAM check_eigen
     REAL, DIMENSION(5,5) :: A
     REAL, DIMENSION(5) :: W
 
-    A(1,:) = [1.0, 0.0, 0.0, 0.0, 0.0]
-    A(2,:) = [0.0, 9.0, 0.0, 0.0, 0.0]
-    A(3,:) = [0.0, 0.0, -2.3, 0.0, 0.0]
-    A(4,:) = [0.0, 0.0, 0.0, 91.0, 0.0]
-    A(5,:) = [0.0, 0.0, 0.0, 0.0, 0.05]
+    A(:,1) = [1.0, 0.0, 0.0, 0.0, 0.0]
+    A(:,2) = [2.0, 9.0, 0.0, 0.0, 0.0]
+    A(:,3) = [-3.0, 0.0, -2.3, 0.0, 0.0]
+    A(:,4) = [75.0, 0.0, 0.0, 91.0, 0.0]
+    A(:,5) = [-0.01, 1.0, 21.0, -107.0, 0.05]
 
-    CALL LA_SYEVX(A,W,IU=1)
+    !CALL LA_SYEVX(A,W,IU=1)
+    CALL LA_SYEVR(A,W)
 
     PRINT*, W
 
